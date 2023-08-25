7d1d0a9c3c5f2615542999d9d8a84cb2af7106d9
Tomashevskyi Vladyslav <113309017+Tomashevskyi-Vladyslav@users.noreply.github.com>
2023-08-25 13:47:39 +0300

    Update Parsing.py

--- Parsing/Parsing.py
+++ Parsing/Parsing.py
@@ -2,18 +2,16 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
 
-url = r''#Your Link
+url = r'https://www.re-store.ru/catalog/MYD82RU-A/'
 
-
-def get_html_new(url=None):
-    browser = webdriver.Chrome()
-    browser.get(str(url))
-    html = browser.page_source
-    time.sleep(2)
+def get_html_new(url):
+    with webdriver.Chrome() as browser:
+        browser.get(url)
+        time.sleep(2)
+        html = browser.page_source
     return html
 
-
-html = get_html_new(url=url)
-soup = BeautifulSoup(html, 'lxml')
-teg = soup.find("body")
-print(teg)
+html = get_html_new(url)
+soup = BeautifulSoup(html, 'html.parser')
+tag = soup.find("body")
+print(tag)
